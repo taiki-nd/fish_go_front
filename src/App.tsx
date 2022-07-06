7fd447f0e311bb3d5c0783ed9e1ed7d0135ad93f
taiki-nd <kii.develop.0517@gmail.com>
2022-07-07 00:12:50 +0900

    ヘッダー、フッターの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <>
+    <div className="container">
+      <header className="blog-header py-3">
+        <div className="row flex-nowrap justify-content-between align-items-center">
+          <div className="col-4 text-center">
+            <a className="fs-1 text-dark" href="#">FishGo</a>
+          </div>
+        </div>
       </header>
+
+      <div className="nav-scroller py-1 mb-2">
+        <nav className="nav d-flex justify-content-between">
+          <a className="p-2 link-secondary" href="#">FreePost</a>
+          <a className="p-2 link-secondary" href="#">AreaData</a>
+          <a className="p-2 link-secondary" href="#">Form</a>
+          <a className="p-2 link-secondary" href="#">FreePost</a>
+          <a className="p-2 link-secondary" href="#">AreaData</a>
+          <a className="p-2 link-secondary" href="#">Form</a>
+        </nav>
+      </div>
     </div>
+
+    <main className="container">
+
+    </main>
+
+    <footer className="blog-footer">
+      <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+    </>
   );
 }
 
