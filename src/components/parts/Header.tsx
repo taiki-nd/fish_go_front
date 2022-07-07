c6d32a9262fa7cd8fa02fe7e8ce9f31bb07762ca
taiki-nd <kii.develop.0517@gmail.com>
2022-07-07 23:35:20 +0900

    headerコンポーネントの作成

---
+++ src/components/parts/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Header = () => {
+  return(
+    <>
+      <header className="blog-header py-3">
+        <div className="row flex-nowrap justify-content-between align-items-center">
+          <div className="col-4 text-center">
+            <a className="fs-1 text-dark" href="#">FishGo</a>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
