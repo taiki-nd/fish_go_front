2f1cffb7e7b4a8b4d191da9ccdb985d0ef4fc433
taiki-nd <kii.develop.0517@gmail.com>
2022-07-12 01:14:20 +0900

    ルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,26 @@ import './App.css';
 import { Header } from './components/parts/Header';
 import { Menu } from './components/parts/Menu'
 import { Footer } from './components/parts/Footer';
+import { Posts } from './components/pages/Posts';
+import { Form } from './components/pages/Form';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div className="container">
-        <Header />
-        <Menu />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Menu />
+        </div>
 
-      <main className="container">
-
-      </main>
+        <main className="container">
+            <Routes>
+              <Route path="/" element={<Posts />} />
+              <Route path="/form" element={<Form />} />
+            </Routes>
+        </main>
+      </BrowserRouter>
 
       <Footer />
     </>
