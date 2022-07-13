db699c6ae7349cbc4237eea47046cc76e5d982dd
taiki-nd <kii.develop.0517@gmail.com>
2022-07-13 23:31:24 +0900

    post index

--- src/components/pages/posts/Posts.tsx
+++ src/components/pages/posts/Posts.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Post } from "../../../model/Post";
 import axios from "axios";
 
 export const Posts = () => {
@@ -8,8 +9,11 @@ export const Posts = () => {
     const index = async () => {
       try {
         const { data } = await axios.get('/posts');
-        console.log('data', data);
-        setPosts(data)
+        console.log(data);
+        if (!data.status) {
+          console.log("failed to get posts");
+        }
+        setPosts(data.data);
       } catch (e: any) {
         console.log('error', e.message);
       }
@@ -23,20 +27,24 @@ export const Posts = () => {
 
       <div className="row mb-2">
 
-        <div className="col-md-6">
-          <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-primary">World</strong>
-              <h3 className="mb-0">Featured post</h3>
-              <div className="mb-1 text-muted">Nov 12</div>
-              <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="stretched-link">Continue reading</a>
+        {posts.map((p: Post) => {
+          return(
+            <div className="col-md-6" key={p.id}>
+              <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style={{backgroundImage: `url(${p.image_url})`}}>
+                <div className="col p-4 d-flex flex-column position-static">
+                  <strong className="d-inline-block mb-2 text-primary">{p.name}</strong>
+                  <h3 className="mb-0">Featured post</h3>
+                  <div className="mb-1 text-muted">{p.updated_at.slice(0, 10)}</div>
+                  <p className="card-text mb-auto">{p.content.substring(0, 30) + " ..."}</p>
+                  <a href="#" className="stretched-link">Continue reading</a>
+                </div>
+                <div className="col-auto d-none d-lg-block">
+                  <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                </div>
+              </div>
             </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            </div>
-          </div>
-        </div>
+          );
+        })}
 
       </div>
     </>
