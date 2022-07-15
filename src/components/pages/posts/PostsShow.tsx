2db3fb27d912c15520acb488273dcf89c1bf29b3
taiki-nd <kii.develop.0517@gmail.com>
2022-07-16 02:42:05 +0900

    レコード情報の取得

--- src/components/pages/posts/PostsShow.tsx
+++ src/components/pages/posts/PostsShow.tsx
@@ -4,6 +4,10 @@ import { useParams } from "react-router-dom";
 
 export const PostsShow = () => {
   const [post, setPost] = useState([]);
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState("");
+  const [updatedAt, setUpdatedAt] = useState("");
 
   const { id } = useParams();
   const getId = () => {
@@ -16,6 +20,11 @@ export const PostsShow = () => {
         const id = getId();
         const { data } = await axios.get(`/posts/${id}`);
         console.log(data);
+        setPost(data.data);
+        setName(data.data.name);
+        setContent(data.data.content);
+        setImage(data.data.image_url);
+        setUpdatedAt(data.data.updated_at);
         if (!data.status) {
           console.log("failed to get posts");
         }
@@ -30,6 +39,11 @@ export const PostsShow = () => {
   return(
     <>
       <h1>Post</h1>
+      {updatedAt.slice(0, 10)}
+      {name}
+      {content}
+      <img src={image} className="" />
+      {console.log(post)}
     </>
   );
 }
